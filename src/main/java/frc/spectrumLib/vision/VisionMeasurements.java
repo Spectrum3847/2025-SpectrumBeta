881dca3e1bc9cb06fa13aee5bbbef8aa211f7b7a
AllenGregoryIV <AllenGregoryIV@gmail.com>
2024-11-21 22:28:44 -0600

    ideas for vision stuff

---
+++ src/main/java/frc/spectrumLib/vision/VisionMeasurements.java
@@ -0,0 +1,13 @@
+package frc.spectrumLib.vision;
+
+public class VisionMeasurements {
+    // Targets in view?
+    // Target Size? reject if target is too small
+    // ambiguity?
+    // Pose estimate (is it off the field, in a place where we can't be?, is the robot tilted, or
+    // floating in air,)
+    // Robot rotating speed
+
+    // If using megaTag2 don't integrate angle at all, since it's using our gyro angle
+
+}
